eeec34ba88ab80b44ad4bc4d4eb1af67d2c80b3b
Hoesung Jung <jrps2212@gmail.com>
2020-09-19 12:06:40 +0900

    Initial commit

---
+++ Recursion_Product of Two Numbers.py
@@ -0,0 +1,14 @@
+x = 5
+y = 3
+
+def recursive_multiply(x, y):
+
+    if x < y:
+        return recursive_multiply(y, x)
+
+    if y == 0:
+        return 0
+    return x + recursive_multiply(x, y-1)
+
+print(x * y)
+print(recursive_multiply(x,y))
